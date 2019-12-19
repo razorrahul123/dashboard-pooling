81940a0a935212659283434c01f042ecdf64c179
razorrahul123 <rahul.kr.sushant@gmail.com>
2019-12-19 17:21:47 +0530

    Updates

---
+++ precache-manifest.10c922ca2e06b3c320bceadf6ef1e723.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc4d22ce775bf0c6cc11f6addbd8c4a",
+    "url": "/dashboard-pooling/index.html"
+  },
+  {
+    "revision": "05796b6ac84669f030d5",
+    "url": "/dashboard-pooling/static/css/main.cb737d3f.chunk.css"
+  },
+  {
+    "revision": "0a55261fefcbbd41507d",
+    "url": "/dashboard-pooling/static/js/2.b0dd5ca0.chunk.js"
+  },
+  {
+    "revision": "eaaf359e6284af6bdda1d61285291759",
+    "url": "/dashboard-pooling/static/js/2.b0dd5ca0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05796b6ac84669f030d5",
+    "url": "/dashboard-pooling/static/js/main.a9d9ea6b.chunk.js"
+  },
+  {
+    "revision": "228a10cc57385ea59fda",
+    "url": "/dashboard-pooling/static/js/runtime-main.8ed85d6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dashboard-pooling/static/media/logo.5d5d9eef.svg"
+  }
+]);
